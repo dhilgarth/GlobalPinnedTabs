b38fc13571c83586c2a25a9ab907ee422c5fbf27
Daniel Hilgarth <d.hilgarth@fire-development.com>
2014-08-24 20:36:29 +0200

    Fix bug with Array.remove when the array contains functions

--- arrayExtensions.js
+++ arrayExtensions.js
@@ -1,21 +1,12 @@
 var ArrayExtensions = {
     init: function() {
         if (!Array.prototype.remove) {
-            Array.prototype.remove = function(valueOrDelegate) {
-                if (valueOrDelegate instanceof Function) {
-                    for (var i = 0; i < this.length; i++) {
-                        if (valueOrDelegate(this[i])) {
-                            this.splice(i, 1);
-                            break;
-                        }
-                    }
-                } else {
-                    var index = this.indexOf(valueOrDelegate);
-                    if (index === -1)
-                        return false;
-                    this.splice(index, 1);
-                    return true;
-                }
+            Array.prototype.remove = function(value) {
+                var index = this.indexOf(value);
+                if (index === -1)
+                    return false;
+                this.splice(index, 1);
+                return true;
             };
         }
 
